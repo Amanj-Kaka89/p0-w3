88f0cc5d1b3f9db5336b17a6792aec8f94299690
unknown <amanj_kakaei@yahoo.com>
2018-07-12 11:13:51 +0700

    add file

---
+++ exercise-3 w3.js
@@ -0,0 +1,15 @@
+
+var input = [
+       ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+       ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+       ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+       ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+  ] 
+  var imput2 =["Nomor ID","Nama Lengkap","TTL","Hobi"];
+  input[0].push("Nomor ID","Nama Lengkap","TTL","Hobi");
+  input[1].push("Nomor ID","Nama Lengkap","TTL","Hobi");
+  input[2].push("Nomor ID","Nama Lengkap","TTL","Hobi")
+  input[3].push("Nomor ID","Nama Lengkap","TTL","Hobi");
+  console.log(input);
+  input[5],[0].sort();
+
