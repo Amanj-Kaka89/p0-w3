9e53064aec0eb38b5f7b99fde202e3d7a897f71d
unknown <amanj_kakaei@yahoo.com>
2018-07-15 23:15:49 +0700

    modified file

--- exercise-3 w3.js
+++ exercise-3 w3.js
@@ -1,15 +1,17 @@
-
-var input = [
-       ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
-       ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
-       ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
-       ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
-  ] 
-  var imput2 =["Nomor ID","Nama Lengkap","TTL","Hobi"];
-  input[0].push("Nomor ID","Nama Lengkap","TTL","Hobi");
-  input[1].push("Nomor ID","Nama Lengkap","TTL","Hobi");
-  input[2].push("Nomor ID","Nama Lengkap","TTL","Hobi")
-  input[3].push("Nomor ID","Nama Lengkap","TTL","Hobi");
-  console.log(input);
-  input[5],[0].sort();
-
+function datahandling(input) {
+    for(var i =0; i<input.length;i++) {
+    console.log("Nomor ID: " + input[i][0]);
+    console.log("Nama Lengkap: " + input[i][1]);
+    console.log("TTL: " + input[i][2]+ input[i][3]);
+    console.log("Hobi: " + input[i][4]);
+    }
+    
+    }
+    
+    var array = [
+                    ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+                    ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+                    ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+                    ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+                ]
+    datahandling(array);
